55894067f2e07c05457becd944a579a640a6abfb
Kryton Bess <krytonbess01@gmail.com>
2024-02-08 14:03:14 -0700

    Formatted pages

--- code.js
+++ code.js
@@ -1,13 +1,31 @@
-function search_words() {
-    let input = document.getElementById("searchbar").value;
-    input = input.toLowerCase();
-    let x = document.getElementsByClassName("words")
+// function search_words() {
+//     let input = document.getElementById("searchbar").value;
+//     input = input.toLowerCase();
+//     let x = document.getElementById("list")
+//     var y = document.getElementsByTagName("li");
     
-    for(i=0; i < x.length; i++) {
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].computedStyleMap.display = "none";
+//     for(i=0; i < y.length; i++) {
+//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
+//             x[i].computedStyleMap.display = "none";
+//         } else {
+//             x[i].computedStyleMap.display = "list-item"
+//         }
+//     }
+// }
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("list");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("p")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
         } else {
-            x[i].computedStyleMap.display = "list-item"
+            li[i].style.display = "none";
         }
     }
 }
